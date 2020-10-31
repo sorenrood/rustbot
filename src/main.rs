4088532e11a33c411f97831659fe9c17d868b6bd
sorenrood <sorenrood@gmail.com>
2020-10-30 17:39:11 -0700

    Added some input code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-pub struct Person {
-    name: String,
-}
-
-impl Person {
-}
-
+use std::io::{stdin,stdout,Write};
 fn main() {
-    let name = String::from("Soren");
-    let soren = Person { name: name };
-    println!("Person's Name: {}", soren.name);
+    let mut s = String::new();
+    print!("Please enter some text: ");
+    let _ = stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n') = s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r') = s.chars().next_back() {
+        s.pop();
+    }
+    println!("You typed: {}", s);
 }
