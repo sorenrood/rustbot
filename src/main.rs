89ae8c7ac2a9dd8815f06d5253f345d7b6cfc447
Soren Rood <sorenrood@gmail.com>
2020-11-02 22:01:36 -0800

    modeled ajay

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ pub mod person;
 use person::{Person, Gender};
 
 fn main() {
-    let name = String::from("Soren");
-    let age: usize = 20;
-    let gender = Gender::Male;
-    let mut soren = Person::new(name, age, gender);
-
+    let soren_name: String = String::from("Soren");
+    let soren_age: usize = 20;
+    let soren_gender: Gender = Gender::Male;
+    let mut soren: Person = Person::new(soren_name, soren_age, soren_gender);
     soren.talk();
+
+    let ajay_name: String = String::from("Ajay");
+    let ajay_age: usize = 19;
+    let ajay_gender: Gender = Gender::Male;
+    let mut ajay: Person = Person::new(ajay_name, ajay_age, ajay_gender);
+    ajay.talk();
 }
