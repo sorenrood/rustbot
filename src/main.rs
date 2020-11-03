fded2424f194612a2dba3eb42f8379973c1eaa86
Soren Rood <sorenrood@gmail.com>
2020-11-02 21:37:45 -0800

    added person class

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-use std::io::{stdin,stdout,Write};
+mod person;
+use person::Person;
+
 fn main() {
-    let mut s = String::new();
-    print!("Please enter some text: ");
-    let _ = stdout().flush();
-    stdin().read_line(&mut s).expect("Did not enter a correct string");
-    if let Some('\n') = s.chars().next_back() {
-        s.pop();
-    }
-    if let Some('\r') = s.chars().next_back() {
-        s.pop();
-    }
-    println!("You typed: {}", s);
+    println!("We are going to construct a cool struct now");
+    let name = String::from("Soren");
+    let age: usize = 20;
+    let mut soren = Person::new(name, age);
+    soren.talk();
 }
---
+++ src/person.rs
@@ -0,0 +1,17 @@
+pub struct Person {
+    name: String,
+    age: usize,
+}
+
+impl Person {
+    pub fn new(n: String, a: usize) -> Person {
+        Person {
+            name: n, 
+            age: a 
+        }
+    }
+
+    pub fn talk(&mut self) {
+        println!("Hi, my name is {}", self.name);
+    }
+}
